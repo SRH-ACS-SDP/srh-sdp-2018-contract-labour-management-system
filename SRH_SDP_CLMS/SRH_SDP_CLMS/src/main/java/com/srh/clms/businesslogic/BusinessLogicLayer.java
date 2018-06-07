a8cbbbb1b6665b3a58a8c62712fb30f12efcc2c9
varad <11010906@STUD.HOCHSCHULE-HEIDELBERG.DE>
2018-06-07 17:19:31 +0200

    add business logic

--- SRH_SDP_CLMS/SRH_SDP_CLMS/src/main/java/com/srh/clms/businesslogic/BusinessLogicLayer.java
+++ SRH_SDP_CLMS/SRH_SDP_CLMS/src/main/java/com/srh/clms/businesslogic/BusinessLogicLayer.java
@@ -1,7 +1,22 @@
 package com.srh.clms.businesslogic;
 import com.srh.clms.dataaccess.*;
+import com.srh.clms.entities.Contractor;
+import com.srh.clms.entities.DetailInfo;
 import com.srh.clms.exception.*;
 
 public class BusinessLogicLayer {
-
+	boolean flag =true;
+	DataAccessLayer dll = new DataAccessLayer();
+	public boolean addContractor(Contractor contractor)
+	{    
+		
+		
+		flag=dll.addContractorDLL(contractor);
+		return flag;
+		
+	}
+	public boolean addDetailinfo(DetailInfo detailInfo)
+	{
+		return flag;
+	}
 }
