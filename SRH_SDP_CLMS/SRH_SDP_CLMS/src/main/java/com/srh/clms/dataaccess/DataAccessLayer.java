dadba10a0f433166fe37b4d2538587c7ac463c87
varad <11010906@STUD.HOCHSCHULE-HEIDELBERG.DE>
2018-06-07 17:22:02 +0200

    add data layer

--- SRH_SDP_CLMS/SRH_SDP_CLMS/src/main/java/com/srh/clms/dataaccess/DataAccessLayer.java
+++ SRH_SDP_CLMS/SRH_SDP_CLMS/src/main/java/com/srh/clms/dataaccess/DataAccessLayer.java
@@ -1,5 +1,7 @@
 package com.srh.clms.dataaccess;
 import com.srh.clms.businesslogic.*;
+import com.srh.clms.entities.Accounts;
+import com.srh.clms.entities.Contractor;
 import com.srh.clms.exception.*;
 
 
@@ -10,4 +12,62 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class DataAccessLayer {
 
+	boolean flag= true;
+
+	protected static SessionFactory sessionfactory;
+
+	protected void setup() 
+	{
+		// A SessionFactory is set up once for an application!
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build(); // configures settings from hibernate.cfg.xml
+
+		try 
+		{
+			sessionfactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		} 
+		catch (Exception e) 
+		{
+			// The registry would be destroyed by the SessionFactory, but we had trouble
+			// building the SessionFactory
+			// so destroy it manually.
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+	
+	protected void exit() 
+	{
+		sessionfactory.close();
+	}
+	
+	
+	public boolean addContractorDLL(Contractor contractor)
+	{
+		flag=true;
+		DataAccessLayer dataAccess= new DataAccessLayer();
+		dataAccess.setup();		
+		org.hibernate.Session session = sessionfactory.openSession();
+	    session.beginTransaction();
+	    session.save(contractor);
+	    session.getTransaction().commit();
+	    session.close();
+	    dataAccess.exit();
+		
+		return flag;
+	}
+	public boolean addAccountDLL(Accounts account)
+	{
+		flag=true;
+		DataAccessLayer dataAccess= new DataAccessLayer();
+		dataAccess.setup();		
+		org.hibernate.Session session = sessionfactory.openSession();
+	    session.beginTransaction();
+	    session.save(account);
+	    session.getTransaction().commit();
+	    session.close();
+	    dataAccess.exit();
+		
+		return flag;
+	}
+	
 }
+
